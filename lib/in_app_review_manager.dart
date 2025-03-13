9d342542b6359c39903d0a5db89d76533503ffdb
eliasteeny <elias.teeny@gmail.com>
2025-03-14 01:24:03 +0200

    wrap all manager functions with try catch and add onfailed callback

--- lib/in_app_review_manager.dart
+++ lib/in_app_review_manager.dart
@@ -19,6 +19,8 @@ class InAppReviewManager {
   static int _minDaysBeforeRemind = 1;
   static int _minSecondsBeforeShowDialog = 1;
 
+  static void Function(Object error, StackTrace stackTrace)? _onFailed;
+
   factory InAppReviewManager() {
     return _singleton;
   }
@@ -26,13 +28,21 @@ class InAppReviewManager {
   bool _isIntermediateDialogInProgress = false;
 
   void monitor() async {
-    if (await _isFirstLaunch() == true) {
-      _setInstallDate();
+    try {
+      if (await _isFirstLaunch() == true) {
+        _setInstallDate();
+      }
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
     }
   }
 
   void applicationWasLaunched() async {
-    _setLaunchTimes(await _getLaunchTimes() + 1);
+    try {
+      _setLaunchTimes(await _getLaunchTimes() + 1);
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+    }
   }
 
   Future<bool> showRateDialogIfMeetsConditions(
@@ -42,10 +52,12 @@ class InAppReviewManager {
     required Widget? intermediateDialogTitle,
     required Widget? intermediateDialogContent,
   }) async {
-    bool isMeetsConditions = await _shouldShowRateDialog();
+    try {
+      bool isMeetsConditions = await _shouldShowRateDialog();
 
-    if (isMeetsConditions) {
-      Future.delayed(Duration(seconds: _minSecondsBeforeShowDialog), () {
+      if (isMeetsConditions) {
+        await Future.delayed(Duration(seconds: _minSecondsBeforeShowDialog));
+
         _showDialog(
           context,
           rateNowButtonText: rateNowButtonText,
@@ -53,9 +65,13 @@ class InAppReviewManager {
           intermediateDialogTitle: intermediateDialogTitle,
           intermediateDialogContent: intermediateDialogContent,
         );
-      });
+      }
+      return isMeetsConditions;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+
+      return false;
     }
-    return isMeetsConditions;
   }
 
   // Setters
@@ -76,6 +92,10 @@ class InAppReviewManager {
     _minSecondsBeforeShowDialog = seconds;
   }
 
+  void setOnFailed(void Function(Object error, StackTrace stackTrace) onFailed) {
+    _onFailed = onFailed;
+  }
+
   // Dialog
   void _showDialog(
     BuildContext? context, {
@@ -84,194 +104,269 @@ class InAppReviewManager {
     required Widget? intermediateDialogTitle,
     required Widget? intermediateDialogContent,
   }) async {
-    final InAppReview inAppReview = InAppReview.instance;
+    try {
+      final InAppReview inAppReview = InAppReview.instance;
 
-    if (context != null && context.mounted) {
-      _isIntermediateDialogInProgress = true;
-      final popValue = await showDialog<_IntermediateDialogState>(
-        context: context,
-        builder: (dialogContext) => AlertDialog(
-          title: intermediateDialogTitle,
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (intermediateDialogContent != null) intermediateDialogContent,
-              const SizedBox(height: 32),
-              Row(
-                children: [
-                  Expanded(
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(
-                          color: Colors.blue,
-                          width: 3,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.of(dialogContext).pop(_IntermediateDialogState.ignore);
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 24),
-                        child: Text(
-                          ignoreButtonText,
-                          style: const TextStyle(
+      if (context != null && context.mounted) {
+        _isIntermediateDialogInProgress = true;
+        final popValue = await showDialog<_IntermediateDialogState>(
+          context: context,
+          builder: (dialogContext) => AlertDialog(
+            title: intermediateDialogTitle,
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (intermediateDialogContent != null) intermediateDialogContent,
+                const SizedBox(height: 32),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
                             color: Colors.blue,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                            width: 3,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(dialogContext).pop(_IntermediateDialogState.ignore);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24),
+                          child: Text(
+                            ignoreButtonText,
+                            style: const TextStyle(
+                              color: Colors.blue,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(
-                          color: Colors.green,
-                          width: 3,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.of(dialogContext).pop(_IntermediateDialogState.rate);
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 24),
-                        child: Text(
-                          rateNowButtonText,
-                          style: const TextStyle(
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
                             color: Colors.green,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                            width: 3,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(dialogContext).pop(_IntermediateDialogState.rate);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24),
+                          child: Text(
+                            rateNowButtonText,
+                            style: const TextStyle(
+                              color: Colors.green,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      );
+        );
 
-      _isIntermediateDialogInProgress = false;
+        _isIntermediateDialogInProgress = false;
 
-      // if popValue is _IntermediateDialogState.rate, resume function normally
-      if (popValue == null || popValue == _IntermediateDialogState.later) {
-        _setRemindTimestamp();
-        return;
-      } else if (popValue == _IntermediateDialogState.ignore) {
-        await _setIsIgnored(true);
-        return;
+        // if popValue is _IntermediateDialogState.rate, resume function normally
+        if (popValue == null || popValue == _IntermediateDialogState.later) {
+          _setRemindTimestamp();
+          return;
+        } else if (popValue == _IntermediateDialogState.ignore) {
+          await _setIsIgnored(true);
+          return;
+        }
       }
+      // _showDialog can only be called if inAppReview.isAvailable() returned true
+
+      inAppReview.requestReview();
+
+      _setRemindTimestamp();
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
     }
-    // _showDialog can only be called if inAppReview.isAvailable() returned true
-
-    inAppReview.requestReview();
-
-    _setRemindTimestamp();
   }
 
   // Checkers
 
   Future<bool> _shouldShowRateDialog() async {
-    final InAppReview inAppReview = InAppReview.instance;
-    return await _isOverLaunchTimes() &&
-        await _isOverInstallDate() &&
-        await _isOverRemindDate() &&
-        await _isNotIgnored() &&
-        await inAppReview.isAvailable() &&
-        !_isIntermediateDialogInProgress;
+    try {
+      final InAppReview inAppReview = InAppReview.instance;
+      return await _isOverLaunchTimes() &&
+          await _isOverInstallDate() &&
+          await _isOverRemindDate() &&
+          await _isNotIgnored() &&
+          await inAppReview.isAvailable() &&
+          !_isIntermediateDialogInProgress;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   Future<bool> _isOverLaunchTimes() async {
-    bool overLaunchTimes = await _getLaunchTimes() >= _minLaunchTimes;
-    return overLaunchTimes;
+    try {
+      bool overLaunchTimes = await _getLaunchTimes() >= _minLaunchTimes;
+      return overLaunchTimes;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   Future<bool> _isOverInstallDate() async {
-    bool overInstallDate = await _isOverDate(await _getInstallTimestamp(), _minDaysAfterInstall);
-    return overInstallDate;
+    try {
+      bool overInstallDate = await _isOverDate(await _getInstallTimestamp(), _minDaysAfterInstall);
+      return overInstallDate;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   Future<bool> _isOverRemindDate() async {
-    bool overRemindDate = await _isOverDate(await _getRemindTimestamp(), _minDaysBeforeRemind);
-    return overRemindDate;
+    try {
+      bool overRemindDate = await _isOverDate(await _getRemindTimestamp(), _minDaysBeforeRemind);
+      return overRemindDate;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   Future<bool> _isNotIgnored() async {
-    bool isIgnored = await _getIsIgnored();
-    return !isIgnored;
+    try {
+      bool isIgnored = await _getIsIgnored();
+      return !isIgnored;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   // Helpers
 
   Future<bool> _isOverDate(int targetDate, int threshold) async {
-    return DateTime.now().millisecondsSinceEpoch - targetDate >= threshold * 24 * 60 * 60 * 1000;
+    try {
+      return DateTime.now().millisecondsSinceEpoch - targetDate >= threshold * 24 * 60 * 60 * 1000;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   // Shared Preference Values
 
   Future<bool> _isFirstLaunch() async {
-    final prefs = await SharedPreferences.getInstance();
-    final int? installDate = prefs.getInt(_prefKeyInstallDate);
-    return installDate == null;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final int? installDate = prefs.getInt(_prefKeyInstallDate);
+      return installDate == null;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   Future<int> _getInstallTimestamp() async {
-    final prefs = await SharedPreferences.getInstance();
-    final int? installTimestamp = prefs.getInt(_prefKeyInstallDate);
-    if (installTimestamp != null) {
-      return installTimestamp;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final int? installTimestamp = prefs.getInt(_prefKeyInstallDate);
+      if (installTimestamp != null) {
+        return installTimestamp;
+      }
+      return 0;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return 0;
     }
-    return 0;
   }
 
   void _setInstallDate() async {
-    final prefs = await SharedPreferences.getInstance();
-    int timestamp = DateTime.now().millisecondsSinceEpoch;
-    prefs.setInt(_prefKeyInstallDate, timestamp);
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      int timestamp = DateTime.now().millisecondsSinceEpoch;
+      prefs.setInt(_prefKeyInstallDate, timestamp);
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+    }
   }
 
   Future<int> _getLaunchTimes() async {
-    final prefs = await SharedPreferences.getInstance();
-    final int? launchTimes = prefs.getInt(_prefKeyLaunchTimes);
-    if (launchTimes != null) {
-      return launchTimes;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final int? launchTimes = prefs.getInt(_prefKeyLaunchTimes);
+      if (launchTimes != null) {
+        return launchTimes;
+      }
+      return 0;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return 0;
     }
-    return 0;
   }
 
   static void _setLaunchTimes(int launchTimes) async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setInt(_prefKeyLaunchTimes, launchTimes);
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setInt(_prefKeyLaunchTimes, launchTimes);
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+    }
   }
 
   Future<int> _getRemindTimestamp() async {
-    final prefs = await SharedPreferences.getInstance();
-    final int? remindIntervalTime = prefs.getInt(_prefKeyRemindInterval);
-    if (remindIntervalTime != null) {
-      return remindIntervalTime;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final int? remindIntervalTime = prefs.getInt(_prefKeyRemindInterval);
+      if (remindIntervalTime != null) {
+        return remindIntervalTime;
+      }
+      return 0;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return 0;
     }
-    return 0;
   }
 
   void _setRemindTimestamp() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.remove(_prefKeyRemindInterval);
-    prefs.setInt(_prefKeyRemindInterval, DateTime.now().millisecondsSinceEpoch);
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.remove(_prefKeyRemindInterval);
+      prefs.setInt(_prefKeyRemindInterval, DateTime.now().millisecondsSinceEpoch);
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+    }
   }
 
   Future<bool> _getIsIgnored() async {
-    final prefs = await SharedPreferences.getInstance();
-    final bool? isIgnored = prefs.getBool(_prefKeyIsIgnored);
-    return isIgnored ?? false;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final bool? isIgnored = prefs.getBool(_prefKeyIsIgnored);
+      return isIgnored ?? false;
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+      return false;
+    }
   }
 
   Future<void> _setIsIgnored(bool isIgnored) async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.remove(_prefKeyIsIgnored);
-    prefs.setBool(_prefKeyIsIgnored, isIgnored);
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.remove(_prefKeyIsIgnored);
+      prefs.setBool(_prefKeyIsIgnored, isIgnored);
+    } catch (error, stackTrace) {
+      _onFailed?.call(error, stackTrace);
+    }
   }
 }
